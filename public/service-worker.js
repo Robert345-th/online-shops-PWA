81148e0c989a20845a75a00de338d472f53e479b
Robert345-th <zulur331@gmail.com>
2026-07-27 14:24:30 +0200

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "zedmarket-v1";
+const PRECACHE = ["/", "/manifest.json"];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
